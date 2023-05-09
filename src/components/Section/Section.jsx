be079719e67ecd4cdc805945ba5f2a8951e8c63c
Oakmoater <dubrovecva@gmail.com>
2023-05-09 21:35:10 +0200

    +;

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -2,12 +2,12 @@ import PropTypes from 'prop-types';
 
 const Section = ({ title, children }) => {
     return (
-    <section>
-        <h2>{title}</h2>
-        {children}
-    </section>
+        <section>
+            <h2>{title}</h2>
+            {children}
+        </section>
     )
-}
+};
 
 Section.propTypes = {
     title: PropTypes.string.isRequired,
